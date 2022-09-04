439dbc1bbba0f92c33c893bd73e92bb8dc6c998d
imabedalghafer <ibabedal@microsoft.com>
2022-09-04 19:11:11 +0300

    updated quorum settings check too

--- cluster-checker.py
+++ cluster-checker.py
@@ -62,7 +62,7 @@ def totemChecker(path_to_scc):
     totem_config = str(output.communicate()[0])
     totem_config = totem_config.replace('b\'totem ','\'')
     logger.info(f'Totem config is {totem_config}')
-    totem_config = eval(totem_config)
+    totem_config = ast.literal_eval(totem_config)
     totem_config = totem_config.replace('interface', 'interface: ')
     totem_config = totem_config.replace('\tringnumber','ringnumber')
     totem_config = totem_config.replace('\tmcastport','mcastport')
@@ -82,30 +82,76 @@ def totemChecker(path_to_scc):
         #    logger.info(i)    
     logger.info(type(totem_config_dict))
     logger.info(totem_config_dict)
+    
     totem_error_list=[]
     logger.info('start checking and comparing against the documentation: https://docs.microsoft.com/en-us/azure/virtual-machines/workloads/sap/high-availability-guide-suse-pacemaker 15[A]')
     if totem_config_dict['token'].strip() != '30000':
-        logger.info(f"Token value is incorrect, customer have this value {totem_config_dict['token'].stripe()}")
-        totem_error_list.apped('token')
-    if totem_config_dict['token_retransmits_before_loss_const'].stripe() != '10':
-        logger.info(f"Token retransmits value is incorrect, customer have this value {totem_config_dict['token_retransmits_before_loss_const'].stripe()}")
+        logger.info(f"Token value is incorrect, customer have this value {totem_config_dict['token'].strip()}")
+        totem_error_list.append('token')
+    if totem_config_dict['token_retransmits_before_loss_const'].strip() != '10':
+        logger.info(f"Token retransmits value is incorrect, customer have this value {totem_config_dict['token_retransmits_before_loss_const'].strip()}")
         totem_error_list.append('token_retransmits_before_loss_const')
-    if totem_config_dict['join'].stripe() != '10':
-        logger.info(f"Join value is incorrect, customer have this value {totem_config_dict['join'].stripe()}")
+    if totem_config_dict['join'].strip() != '60':
+        logger.info(f"Join value is incorrect, customer have this value {totem_config_dict['join'].strip()}")
         totem_error_list.append('join')
-    if totem_config_dict['consensus'].stripe() != '36000':
-        logger.info(f"consensus value is incorrect, customer have this value {totem_config_dict['consensus'].stripe()}")
+    if totem_config_dict['consensus'].strip() != '36000':
+        logger.info(f"consensus value is incorrect, customer have this value {totem_config_dict['consensus'].strip()}")
         totem_error_list.append('consensus')
-    if totem_config_dict['max_messages'].stripe() != '20':
-        logger.info(f"max_messages value is incorrect, customer have this value {totem_config_dict['max_messages'].stripe()}")
+    if totem_config_dict['max_messages'].strip() != '20':
+        logger.info(f"max_messages value is incorrect, customer have this value {totem_config_dict['max_messages'].strip()}")
         totem_error_list.append('max_messages')
-    if totem_config_dict['transport'].stripe() != '20':
-        logger.info(f"transport value is incorrect, customer have this value {totem_config_dict['transport'].stripe()}")
+    if totem_config_dict['transport'].strip() != 'udpu':
+        logger.info(f"transport value is incorrect, customer have this value {totem_config_dict['transport'].strip()}")
         totem_error_list.append('transport')
 
     if len(totem_error_list) != 0:
         print(f'We found the below issues in the totem configuration of cluster, the below parameters needs to be checked {totem_error_list}')
+        logger.info(f'We found the below issues in the totem configuration of cluster, the below parameters needs to be checked {totem_error_list}')
+    else:
+        print('Done checking on totem configuration, and no error found... proceeding further')
+        logger.info('Done with totem check')
 
+
+def quorumChecker(path_to_scc):
+    path_to_ha = path_to_scc + '/ha.txt'
+    quorum_cmd = 'grep  -A 7 "quorum {" ' + path_to_ha + ' | grep -v "#" ' 
+    output = subprocess.Popen([quorum_cmd], stdout= subprocess.PIPE, shell=True)
+    quorum_config = str(output.communicate()[0])
+    logger.info(f'Quorum configuration is: {quorum_config}')
+    quorum_config = quorum_config.replace('b\'quorum', '\'')
+    quorum_config = ast.literal_eval(quorum_config)
+    quorum_config = quorum_config.split('\n\t')
+    quorum_config_dict = dict()
+    for i in quorum_config:
+        if i.find(':') != -1:
+            temp_list = i.split(':')
+            if temp_list[1].find('}') != -1:
+                temp_list[1] = temp_list[1].strip().replace('}','')
+                #logger.info(temp_list[1])
+            #logger.info(temp_list[1])    
+            quorum_config_dict[temp_list[0]] = temp_list[1].strip()
+            #logger.info(i.split(':'))
+    logger.info(quorum_config_dict)
+    logger.info('start checking Quorum setting in corosync based on our documentation')
+    quorom_error_list = []
+    if quorum_config_dict['provider'].strip() != 'corosync_votequorum':
+        logger.info(f"provider value is incorrect, customer have this value {quorum_config_dict['provider'].strip()}")
+        quorom_error_list.append('provider')
+    if quorum_config_dict['expected_votes'].strip() != '2':
+        logger.info(f"expected_votes value is incorrect, customer have this value {quorum_config_dict['expected_votes'].strip()}")
+        quorom_error_list.append('expected_votes')
+    if quorum_config_dict['two_node'].strip() != '1':
+        logger.info(f"two_node value is incorrect, customer have this value {quorum_config_dict['two_node'].strip()}")
+        quorom_error_list.append('two_node')
+    
+    if len(quorom_error_list) != 0:
+        print(f'We found the below issues in the Quorum configuration of cluster, the below parameters needs to be checked {quorom_error_list}')
+        logger.info(f'We found the below issues in the Quorum configuration of cluster, the below parameters needs to be checked {quorom_error_list}')
+    else:
+        print('Done checking on Quorum configuration, and no error found... proceeding further')
+        logger.info('Done with Quorum check')
+    
+    
 
 if __name__ == '__main__':
     raw_args = sys.argv
@@ -123,6 +169,7 @@ if __name__ == '__main__':
     logger.info(path_to_scc)
     if checkFileExistance(path_to_scc):
         totemChecker(path_to_scc)
+        quorumChecker(path_to_scc)
 
 
 
